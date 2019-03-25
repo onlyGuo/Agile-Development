8c23f7030f381c3abc06ffb84f3188e01ff3a53a
gsk <guosk@lianjintai.com>
2019-03-25 12:47:04 +0800

    添加WEB公共文件

---
+++ agile-dev/src/test/java/com/xhd/aiyi/wiki/wiki/AgileDevApplicationTest.java
@@ -0,0 +1,17 @@
+package com.xhd.aiyi.wiki.wiki;
+
+import com.aiyi.tool.dev.AgileDevApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = AgileDevApplication.class)
+public class AgileDevApplicationTest {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
